3cc54b8766f3d18c561659673a6515ebfa0cdac6
Rlohaw <rlohaw@yandex.ru>
2022-09-23 23:50:12 +0300

    Restructured, optimized

--- main.py
+++ main.py
@@ -1,93 +1,95 @@
+import collections
 import locale
-import time
-from datetime import datetime
-import zipfile
+import os
 import re
+import time
+import zipfile
 
 locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
-name = input('Archive Path: ').strip()
+name = input('Укажите путь к архиву: ').strip()
 if not name.endswith('.zip'):
-    name+='.zip'
+    name += '.zip'
 
-with zipfile.ZipFile(name, mode='r') as zp:
-    path = [i.filename for i in zp.infolist() if not i.is_dir()]
-    modes = '\n'.join(sorted(set(i.split('/', 1)[0] for i in path if '.' not in i.split('/', 1)[0])))
+with zipfile.ZipFile(name, mode='r') as zp:  # Открываем зип
 
-    mode = input('\nDATA SETS:\n'+modes+'\nSELECT: ').strip()
-    filtred_files = list(filter(lambda x:x.split('/', 1)[0] == mode, path))
+    def clear_console():  # очистка экрана от ненужного мусора
+        os.system('cls')
 
-    if mode == 'ads':
 
-        def ads_cord():
-            for i in filtred_files:
-                if 'geo-points' in i:
-                    with zp.open(i) as vrfl:
-                        pr = vrfl.read().decode('windows-1251', "ignore")
-                        pr = [i.replace('\n\n', '') for i in pr.split()]
-                        GM = list(filter(lambda x: 'google' in x, pr))[0].replace('href="','').replace('">:','').replace('">Широта','')
-                        cord = GM[GM.index('=')+1:].replace('">Широта','').split(',')
-                        print(f'\nPopular place:\nCoordinates: {", ".join(cord)}\nGoogle Maps: {GM}\n')
-                        break
+    def opened_file(key):
+        with zp.open(*(i for i in path if key in i)) as file:
+            return file
 
-        def ads_interests():
-            for i in filtred_files:
-                if 'interests' in i:
-                    with zp.open(i) as vrfl:
-                        pr = vrfl.read().decode('windows-1251', "ignore")
-                        text = pr.split('</h2><div class="wrap_page_content">\n  <div class="item">\n')[1].replace('\n  \n  ', '\n').split('</div>\n</div>\n<!--/content-->')[0].rstrip('\n')
-                        mass = [[i.lstrip().split('\n')[0].split("<div class='item__main'>")[1].rstrip('</div>'), i.lstrip().split('\n')[1].split("<div class='item__tertiary'>")[1].rstrip('</div>')][::-1] for i in text.split('\n</div><div class="item">\n')]
-                        dc = {}
-                        for i in mass:
-                            dc[i[0]] = dc.get(i[0],'') + i[1]+'\n'
-                        q = input('\nChoose Category:\nui: User Interests\nos: Other Segment\nall: All Categories\nSELECT: ').strip()
-                        sl = {'ui': 'Пользовательский интерес', 'os': 'Сторонний сегмент', 'all': False}
-                        if sl[q]:
-                            print(dc[sl[q]])
-                        else:
-                            print(f'\nПользовательский интерес:\n{dc["Пользовательский интерес"]}\nСторонний сегмент:\n{dc["Сторонний сегмент"]}')
-                        break
 
-        chs = input('\nACTIONS:\ncrd: Coordinates\nint: Interests\nSELECT: ').strip()
-        if chs == 'crd':
+    def time_counter(f):  # Отладка по времени(декоратор)
+        def wrapper(*args, **kwargs):
+            start = time.perf_counter()
+            a = f(*args, **kwargs)
+            end = time.perf_counter()
+            print(end - start, 'c')
+            return a
+
+        return wrapper
+
+
+    def ads():  # раздел рекламы
+        def ads_cord():  # раздел частая геопозиция
+            text = opened_file('geo-points').read().decode('ANSI')
+            geo = re.search('<a href="(?P<site>.+)">(?P<lat>.+), (?P<lon>.+)</a>', text)
+            print(f"Геопозиция на картах:{geo['site']}\n{geo['lat']}\n{geo['lon']}")
+
+        def ads_interests():  # раздел интересы
+            def user_interests():
+                print(*d['Пользовательский интерес'], sep='\n')
+
+            def other_segments():
+                print(*d['Сторонний сегмент'], sep='\n')
+
+            def all_interests():
+                print('Пользовательский интерес:')
+                user_interests()
+                print('Сторонний сегмент:')
+                other_segments()
+
+            text = opened_file('interests').read().decode('ANSI')
+            mass = re.findall('''item__main'>([А-яA-z0-9/ -–—,.()]+)</div>|item__tertiary'>([А-я ]+)</div>''', text)
+            mass = list(map(lambda x: ''.join(x), mass))
+            d = collections.defaultdict(list)
+            [d[mass[i + 1]].append(mass[i]) for i in range(len(mass) - 1)]
+
+            ads_interests_print_modes = list(enumerate(['Пользовательский интерес',
+                                                        'Сторонний сегмент',
+                                                        'Оба варианта']))
+            ads_interests_mode_selectors = {0: user_interests, 1: other_segments, 2: all_interests}
+
+            [print(f"{i[0]}: {i[1]}") for i in ads_interests_print_modes]
+            ads_interests_mode_selectors[int(input('Выберите класс: '))]()
+
+        def ads_all():
+            print()
             ads_cord()
-        elif chs == 'int':
+            print()
             ads_interests()
 
-    elif mode == 'apps':
-        for i in filtred_files:
-            if 'apps' in i:
-                with zp.open(i) as vrfl:
-                    pr = vrfl.read().decode('windows-1251', "ignore")
-                    text = pr.split('</h2><div class="wrap_page_content">\n  <div class="item">\n')[1].split('</div>\n</div>\n<!--/content-->')[0].rstrip('\n')
-                    md = input("\nShow applications? y/n: ")
-                    mass = [[i.lstrip().split('\n')[0].split("<div class='item__main'>")[1].rstrip('</div>'), i.split('\n')[2].lstrip().lstrip("<div class='item__tertiary'>").split("</div><div class='item__tertiary'>Дата последнего запуска неизвестна</div>")[0]] for i in text.split('\n</div>\n<div class="item">\n')]
-                    if md == 'y':
-                        mass = list(map(lambda x: '\n'.join(x), mass))
-                        print()
-                        print(*mass, sep='\n\n')
-                    elif md == 'n':
-                        mass = list(map(lambda x:x[0], mass))
-                        print(*mass, sep='\n')
-                    break
-
-    elif mode == 'audio':
-        for i in filtred_files:
-            if 'audios0' in i:
-                with zp.open(i) as vrfl:
-                    pr = vrfl.read().decode('windows-1251', "ignore")
-                    print('Under Development')
-    elif mode == 'bookmarks':
-        print('Under Development')
-    elif mode == 'likes':
-        print('Under Development')
-
-    # elif mode == 'messages':
+        ads_print_mode = list(enumerate(['Координаты',
+                                         'Интересы',
+                                         'Все режимы']))
+        [print(f"{i[0]}: {i[1]}") for i in ads_print_mode]
+        ads_mode_selector = {0: ads_cord, 1: ads_interests, 2: ads_all}
+        ads_mode_selector[int(input('Выберите режим: '))]()
 
 
+    def apps():
+        pass
 
 
+    ####################################################################################################################
+    path = [i.filename for i in zp.infolist() if not i.is_dir()]  # Составляем список файлов
+    mask = r'(?:\.\w+)?/.+|^(?:\w+\.\w+)$'
+    modes = list(enumerate(sorted(set(re.sub(mask, '', i) for i in path if
+                                      re.sub(mask, '', i)))))  # Создаем список папок и проверяем не пустое ли имя
+    dmodes = dict(modes)  # Создаем словарь с папками
+    [print(f"{i[0]}: {i[1]}") for i in modes]
 
-
-
-
-
+    funcs = {0: ads, 1: apps}
+    funcs[int(input('Выборите данные с которыми хотите работать: '))]()
